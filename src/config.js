41c7eaaa5fbd109ca1de73ceb39ec286fbb309a3
Techfcf <tech@fcfindia.in>
2026-04-08 14:45:41 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ const isLocalhost = typeof window !== "undefined" &&
   (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1");
 
 export const API_BASE_URL = isLocalhost
-  ? "http://localhost:8083"
+  ? "https://mrv-backend-13977221722.asia-south1.run.app"
   : "https://mrv-backend-13977221722.asia-south1.run.app";
